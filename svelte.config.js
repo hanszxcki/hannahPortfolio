4a0224818c74961232df47d79d1d8e9e937f62cc
hanszxcki <115547443+hanszxcki@users.noreply.github.com>
2023-10-31 18:01:29 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-        base: process.env.NODE_ENV === 'production' ? '/hannahPortfolio-github-pages' : '',
+        base: process.env.NODE_ENV === 'production' ? '/svelte-github-pages' : '',
     }
   },
   preprocess: vitePreprocess()
